1118715f2f26be9a99cbae7be776ae7902343d2a
Gabriel044 <gabgar4@gmail.com>
2022-06-08 11:48:34 -0500

    No need to refresh for bunkers

--- world-war-z/src/components/BunkerForm/bunkerform.js
+++ world-war-z/src/components/BunkerForm/bunkerform.js
@@ -2,39 +2,43 @@ import React, { useRef, useState } from "react";
 import Banner from "../../components/Banner/banner";
 import "./bunkerform.css";
 
-export default function BunkerForm({setAddBunker}){
-    const [showBanner, setShowBanner] = useState(false);
-    const bunkerNameRef = useRef();
-    const latRef = useRef();
-    const longRef = useRef();
-    const checkboxRef = useRef();
-    
-    if (showBanner)
+export default function BunkerForm({ setAddBunker, setBunkers, bunkers }) {
+  const [showBanner, setShowBanner] = useState(false);
+  const bunkerNameRef = useRef();
+  const latRef = useRef();
+  const longRef = useRef();
+  const checkboxRef = useRef();
+
+  if (showBanner)
     setTimeout(() => {
       setShowBanner(false);
     }, 10000);
 
   function addBunker() {
-    let bunkerName=bunkerNameRef.current.value;
-    let lat=latRef.current.value;
-    let long=longRef.current.value;
-    let alive=checkboxRef.current.value;
+    let bunkerName = bunkerNameRef.current.value;
+    let lat = latRef.current.value;
+    let long = longRef.current.value;
+    let alive = checkboxRef.current.value;
     /* TODO: Agrega un poco más de validación de datos como:  
       1. Rango válido de valores para latitud y longitud 
     */
-    if (bunkerName == "" || lat == "" || long == "" ) {
-        setShowBanner(true);
-        return;
-      }
-
-    if (alive === "on") {alive = true} else {alive = false}
-    let data={
-        name:bunkerName,
-        lat:parseFloat(lat),
-        long:parseFloat(long),
-        isAlive:alive
+    if (bunkerName == "" || lat == "" || long == "") {
+      setShowBanner(true);
+      return;
     }
-    console.log("La data del bunker es: \n" + data)
+
+    if (alive === "on") {
+      alive = true;
+    } else {
+      alive = false;
+    }
+    let data = {
+      name: bunkerName,
+      lat: parseFloat(lat),
+      long: parseFloat(long),
+      isAlive: alive,
+    };
+    console.log("La data del bunker es: \n" + data);
 
     fetch("http://192.168.1.17:5000/bunkers", {
       method: "POST",
@@ -45,62 +49,59 @@ export default function BunkerForm({setAddBunker}){
     })
       .then((response) => response.json())
       .then((data) => {
+        setBunkers([...bunkers, data]);
         console.log("Success:", data);
       })
       .catch((e) => console.log(e));
-
   }
 
   function cancelPost() {
     setAddBunker(false);
   }
 
-    return (
-        <div id="PostFormContainer">
-          {showBanner && (
-            <Banner status={false} message={"Fill all the text fields"} />
-          )}
-          <input
-            type="text"
-            ref={bunkerNameRef}
-            className="PostFormInput"
-            placeholder="Bunker Name"
-          />
-          <input
-            type="number"
-            ref={latRef}
-            className="PostFormInput"
-            placeholder="Bunker's latitude"
-          />
-          <input
-            type="number"
-            ref={longRef}
-            className="PostFormInput"
-            placeholder="Bunker's longitude"
-          />
-          <label>
-          <input
-            type="checkbox"
-            ref={checkboxRef}
-          />     isAlive
-          </label>
-            
-          <div id="PostFormFooter">
-            <button
-              className="PostFormButton"
-              style={{ backgroundColor: "#4aa0eb" }}
-              onClick={() => addBunker()}
-            >
-              Post
-            </button>
-            <button
-              className="PostFormButton"
-              style={{ backgroundColor: "grey" }}
-              onClick={() => cancelPost()}
-            >
-              Cancel
-            </button>
-          </div>
-        </div>
-    );
+  return (
+    <div id="PostFormContainer">
+      {showBanner && (
+        <Banner status={false} message={"Fill all the text fields"} />
+      )}
+      <input
+        type="text"
+        ref={bunkerNameRef}
+        className="PostFormInput"
+        placeholder="Bunker Name"
+      />
+      <input
+        type="number"
+        ref={latRef}
+        className="PostFormInput"
+        placeholder="Bunker's latitude"
+      />
+      <input
+        type="number"
+        ref={longRef}
+        className="PostFormInput"
+        placeholder="Bunker's longitude"
+      />
+      <label>
+        <input type="checkbox" ref={checkboxRef} /> isAlive
+      </label>
+
+      <div id="PostFormFooter">
+        <button
+          className="PostFormButton"
+          style={{ backgroundColor: "#4aa0eb" }}
+          onClick={() => addBunker()}
+        >
+          Post
+        </button>
+        <button
+          className="PostFormButton"
+          style={{ backgroundColor: "grey" }}
+          onClick={() => cancelPost()}
+        >
+          Cancel
+        </button>
+      </div>
+    </div>
+  );
 }
--- world-war-z/src/routes/BunkerMap/bunkerMap.js
+++ world-war-z/src/routes/BunkerMap/bunkerMap.js
@@ -1,48 +1,49 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./bunkerMap.css";
-import 'mapbox-gl/dist/mapbox-gl.css';
-import mapboxgl from 'mapbox-gl';
+import "mapbox-gl/dist/mapbox-gl.css";
+import mapboxgl from "mapbox-gl";
 import { IoAddCircleSharp } from "react-icons/io5";
 import BunkerForm from "../../components/BunkerForm/bunkerform";
 
-
 export default function BunkerMap() {
-  const [map,setMap]=useState(null);
+  const [map, setMap] = useState(null);
   const [bunkers, setBunkers] = useState([]);
   const [addBunker, setAddBunker] = useState(false);
-  const [updatedMap,setUpdatedMap]=useState(true)
-  console.log('hi:)')
-  
+  const [updatedMap, setUpdatedMap] = useState(true);
+  console.log("hi:)");
+
   useEffect(() => {
-    mapboxgl.accessToken = 'pk.eyJ1IjoicHJ1ZWJhYSIsImEiOiJjbDJ6aHRldzQxZnJ0M2lwMmpmbHM1ZHJjIn0.7usXzE9zv3Oi6NpMMYBqJA'; 
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoicHJ1ZWJhYSIsImEiOiJjbDJ6aHRldzQxZnJ0M2lwMmpmbHM1ZHJjIn0.7usXzE9zv3Oi6NpMMYBqJA";
     //mapa
-    setMap(new mapboxgl.Map({
-      container: 'map', // container ID
-      style: 'mapbox://styles/mapbox/dark-v10', // dark mode
-      //style: 'mapbox://styles/mapbox/streets-v11', // modo claro
-      center: [-103.454810, 20.732498], // [lng, lat] inicia en el tec 
-      zoom: 13 // starting zoom
-    }))
-    getBunkers()
-  },[])
+    setMap(
+      new mapboxgl.Map({
+        container: "map", // container ID
+        style: "mapbox://styles/mapbox/dark-v10", // dark mode
+        //style: 'mapbox://styles/mapbox/streets-v11', // modo claro
+        center: [-103.45481, 20.732498], // [lng, lat] inicia en el tec
+        zoom: 13, // starting zoom
+      })
+    );
+    getBunkers();
+  }, []);
   useEffect(() => {
     if (bunkers.length != 0) {
-      console.log('2:)')
+      console.log("2:)");
       agregarMarcadores(bunkers);
       setUpdatedMap(!updatedMap);
     }
-
-  },[bunkers])
-  function getBunkers(){
+  }, [bunkers]);
+  function getBunkers() {
     fetch("http://192.168.1.17:5000/bunkers")
       .then((res) => {
         res
           .json()
           .then((object) => {
             //console.log(typeof (object.bunkers));
-            var bunkers = JSON.parse(object.bunker) // Convertir object a string
-            let bunkerArray=[]
-            for(var key in bunkers){
+            var bunkers = JSON.parse(object.bunker); // Convertir object a string
+            let bunkerArray = [];
+            for (var key in bunkers) {
               //console.log(bunkers[key]);
               bunkerArray.push(bunkers[key]);
             }
@@ -53,50 +54,58 @@ export default function BunkerMap() {
           .catch((err) => console.log(err));
       })
       .catch((err) => {
-        console.log("Hubo un error al solicitar los bunkers: " + err)
+        console.log("Hubo un error al solicitar los bunkers: " + err);
         // solicitar posts de local storage
         //setPosts(getStoredPosts())
-    });
+      });
   }
 
-
-  
-  const puntosObjeto=[
+  const puntosObjeto = [
     {
-        lat:-103.455739,
-        long:20.735764,
-        isAlive:false
-    },{
-        lat:-103.450320,
-        long: 20.730336,
-        isAlive:true
-    },{
-        lat:-103.442014,
-        long:20.664497,
-        isAlive:true
-    }];
-    
+      lat: -103.455739,
+      long: 20.735764,
+      isAlive: false,
+    },
+    {
+      lat: -103.45032,
+      long: 20.730336,
+      isAlive: true,
+    },
+    {
+      lat: -103.442014,
+      long: 20.664497,
+      isAlive: true,
+    },
+  ];
 
+  function agregarMarcadores(obejeto) {
+    console.log(obejeto);
 
-  function agregarMarcadores(obejeto){
-    console.log(obejeto)
-    
-    for(var i =0;i<obejeto.length;i++){
+    for (var i = 0; i < obejeto.length; i++) {
       //console.log(obejeto[i].isAlive)
       //console.log(obejeto[i].lat)
       //console.log(obejeto[i].long)
-      const marker1 = new mapboxgl.Marker({color: obejeto[i]['isAlive']? 'green':'black', rotation:0})
-      .setLngLat([obejeto[i]['lat'],obejeto[i]['long']])
-      .addTo(map);        
+      const marker1 = new mapboxgl.Marker({
+        color: obejeto[i]["isAlive"] ? "green" : "black",
+        rotation: 0,
+      })
+        .setLngLat([obejeto[i]["lat"], obejeto[i]["long"]])
+        .addTo(map);
     }
   }
-  
-return <div>
-  
-  <div id='map' style={{width: "500px", height: "500px"}}></div>
-  
-  {addBunker && (<BunkerForm setAddBunker={setAddBunker}/>)}
-  <IoAddCircleSharp id="AddPost" onClick={() => setAddBunker(true)} />
 
-  </div>;
+  return (
+    <div>
+      <div id="map" style={{ width: "500px", height: "500px" }}></div>
+
+      {addBunker && (
+        <BunkerForm
+          setAddBunker={setAddBunker}
+          bunkers={bunkers}
+          setBunkers={setBunkers}
+        />
+      )}
+      <IoAddCircleSharp id="AddPost" onClick={() => setAddBunker(true)} />
+    </div>
+  );
 }
